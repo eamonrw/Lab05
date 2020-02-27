75f0d69f4280259c093a390b51bca37950a4d4e2
eamonrw <eamonweingold@gmail.com>
2020-02-27 09:48:20 -0500

    Finished Lifecycle Set 1 resets lifecycle data after each run of app Set 2 is permanent until reset button pressed

--- app/src/main/java/com/weingoldeamon/lab05/MainActivity.java
+++ app/src/main/java/com/weingoldeamon/lab05/MainActivity.java
@@ -1,9 +1,11 @@
 package com.weingoldeamon.lab05;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LifecycleOwner;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.TreeMap;
@@ -12,14 +14,16 @@ public class MainActivity extends AppCompatActivity {
 
     MethodCounter cycleTracker = new MethodCounter();
     TextView onCreate1, onStart1, onResume1, onPause1, onStop1, onRestart1, onDestroy1;
+    TextView onCreate2, onStart2, onResume2, onPause2, onStop2, onRestart2, onDestroy2;
+    TreeMap<String, TextView[]> viewMap = new TreeMap<>();
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getLifecycle().addObserver(cycleTracker);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pref = getSharedPreferences("Lifecycle_Counts", Context.MODE_PRIVATE);
         onCreate1 = findViewById(R.id.onCreate_text1);
         onStart1 = findViewById(R.id.onStart_text1);
         onResume1 = findViewById(R.id.onResume_text1);
@@ -27,7 +31,82 @@ public class MainActivity extends AppCompatActivity {
         onStop1 = findViewById(R.id.onStop_text1);
         onRestart1 = findViewById(R.id.onRestart_text1);
         onDestroy1 = findViewById(R.id.onDestroy_text1);
+        onCreate2 = findViewById(R.id.onCreate_text2);
+        onStart2 = findViewById(R.id.onStart_text2);
+        onResume2 = findViewById(R.id.onResume_text2);
+        onPause2 = findViewById(R.id.onPause_text2);
+        onStop2 = findViewById(R.id.onStop_text2);
+        onRestart2 = findViewById(R.id.onRestart_text2);
+        onDestroy2 = findViewById(R.id.onDestroy_text2);
+        viewMap.put("onCreate", new TextView[]{onCreate1, onCreate2});
+        viewMap.put("onStart", new TextView[]{onStart1, onStart2});
+        viewMap.put("onResume", new TextView[]{onResume1, onResume2});
+        viewMap.put("onPause", new TextView[]{onPause1, onPause2});
+        viewMap.put("onStop", new TextView[]{onStop1, onStop2});
+        viewMap.put("onRestart", new TextView[]{onRestart1, onRestart2});
+        viewMap.put("onDestroy", new TextView[]{onDestroy1, onDestroy2});
+        updateCount("onCreate");
+    }
+    
+    public void updateCount(String methodName) {
+        SharedPreferences.Editor editor = pref.edit();
+        cycleTracker.incrementMethodCount(methodName);
+        editor.putInt(methodName, pref.getInt(methodName, 0) + 1);
+        editor.apply();
+    }
+    public void updateText() {
+        for(String key: viewMap.keySet()) {
+            TextView[] tv = viewMap.get(key);
+            tv[0].setText(""+cycleTracker.getMethodCount(key));
+            tv[1].setText(""+pref.getInt(key, 0));
+        }
+    }
+    @Override
+    protected void onStart() {
+        updateCount("onStart");
+        super.onStart();
+        updateText();
+    }
+    @Override
+    protected void onResume() {
+        updateCount("onResume");
+        super.onResume();
+        updateText();
+    }
+    @Override
+    protected void onPause() {
+        updateCount("onPause");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        updateCount("onStop");
+        super.onStop();
+    }
+    @Override
+    protected void onRestart() {
+        updateCount("onRestart");
+        super.onRestart();
+    }
+    @Override
+    protected void onDestroy() {
+        updateCount("onDestroy");
+        super.onDestroy();
+    }
 
-        TreeMap<String, Integer> eventMap = cycleTracker.getEventMap();
+    public void resetCount1(View view) {
+        for(String key: viewMap.keySet()) {
+            TextView[] tv = viewMap.get(key);
+            tv[0].setText("0");
+        }
+    }
+    public void resetCount2(View view) {
+        for(String key: viewMap.keySet()) {
+            TextView[] tv = viewMap.get(key);
+            tv[1].setText("0");
+            SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            editor.apply();
+        }
     }
 }
--- app/src/main/java/com/weingoldeamon/lab05/MethodCounter.java
+++ app/src/main/java/com/weingoldeamon/lab05/MethodCounter.java
@@ -1,47 +1,23 @@
 package com.weingoldeamon.lab05;
 
-
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.OnLifecycleEvent;
-
 import java.util.TreeMap;
 
-public class MethodCounter implements LifecycleObserver {
+public class MethodCounter {
     private TreeMap<String, Integer> eventMap;
 
     public MethodCounter(){
         eventMap = new TreeMap<>();
-        for(String s: new String[]{"onCreate", "onStart", "onResume", "onPause", "onStop", "onDestroy"})
-            eventMap.put(s, 0);
     }
 
-    public TreeMap getEventMap(){
-        return eventMap;
+    public void incrementMethodCount(String methodName) {
+        int value = 1;
+        if(eventMap.containsKey(methodName))
+            value += eventMap.get(methodName);
+        eventMap.put(methodName, value);
     }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    public void onCreateCount(){
-        eventMap.put("onCreate", eventMap.get("onCreate") + 1);
-    }
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
-    public void onStartCount(){
-        eventMap.put("onStart", eventMap.get("onStart") + 1);
-    }
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    public void onResumeCount(){
-        eventMap.put("onResume", eventMap.get("onResume") + 1);
-    }
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-    public void onPauseCount(){
-        eventMap.put("onPause", eventMap.get("onPause") + 1);
-    }
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-    public void onStopCount(){
-        eventMap.put("onStop", eventMap.get("onStop") + 1);
-    }
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    public void onDestroyCount(){
-        eventMap.put("onDestroy", eventMap.get("onDestroy") + 1);
+    public int getMethodCount(String methodName) {
+        if(eventMap.containsKey(methodName))
+            return eventMap.get(methodName);
+        return 0;
     }
 }
